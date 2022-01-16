9594385dbdeb0973742619882a94102eccece425
Vinicius Ribeiro <vayne99@gmail.com>
2022-01-16 08:15:41 -0300

    Correções no input range

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
 const sliderValue = document.querySelector('.sliderValue')
 const slider = document.querySelector('input')
+const registerButton = document.querySelector('.register')
+const note = document.querySelector('.note-input')
+
 
 slider.oninput = (()=> {
-    let value = slider.value;
+    var value = slider.value;
     sliderValue.classList.add('show');
     sliderValue.style.left = (value/2)+'%'
-    if((value>=0) && (value<=40)) 
+
+    if((value>=20) && (value<=53)) 
     sliderValue.textContent = '😭'
 
-    if((value>40) && (value<=80)) 
+    if((value>53) && (value<=83)) 
     sliderValue.textContent = '😩'
 
-    if((value>80) && (value<=120)) 
+    if((value>83) && (value<=113)) 
     sliderValue.textContent = '😐'
     
-    if((value>120) && (value<=160)) 
+    if((value>113) && (value<=143)) 
     sliderValue.textContent = '😀'
 
-    if((value>160) && (value<=200)) 
+    if((value>143) && (value<=180)) 
     sliderValue.textContent = '🤩'
     
 })
@@ -25,3 +29,16 @@ slider.oninput = (()=> {
 slider.onblur = (()=> {
     sliderValue.classList.remove('show');
 });
+
+registerButton.addEventListener('click', ()=> {
+    var value = slider.value;
+    noteText = note.value
+    console.log(noteText)
+    var dataForSave = {
+        noteText,
+        value
+    }
+
+    localStorage.setItem('data', JSON.stringify(dataForSave))
+
+})
