8a2032d6b080782e7cd8cc2e17b0edc4125ac2ec
Vinicius Ribeiro <vayne99@gmail.com>
2022-01-17 08:52:36 -0300

    Exibição de notas salvas

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const sliderValue = document.querySelector('.sliderValue')
 const slider = document.querySelector('input')
 const registerButton = document.querySelector('.register')
 const note = document.querySelector('.note-input')
-
+const historic = document.querySelector('.historic')
+const previousNotes = document.querySelector('.previous-notes')
+const closeButton = document.querySelector('.close-button')
 
 slider.oninput = (()=> {
     var value = slider.value;
@@ -46,5 +48,57 @@ registerButton.addEventListener('click', ()=> {
     const updateData = [...savedData, dataForSave]
     localStorage.setItem('data', JSON.stringify(updateData))
     note.value = ''
-
+    loadNotes()
 })
+
+historic.addEventListener('click', () => {
+    previousNotes.classList.add('visible')
+    loadNotes()
+})
+
+closeButton.addEventListener('click', () => {
+    previousNotes.classList.remove('visible')
+})
+
+
+const Note = ({ today, noteText, value }) => {
+    const noteLi = document.createElement('li')
+    noteLi.classList.add('note-item')
+    var emoji
+    if((value>=20) && (value<=53)) 
+    emoji = '😭'
+
+    if((value>53) && (value<=83)) 
+    emoji = '😩'
+
+    if((value>83) && (value<=113)) 
+    emoji = '😐'
+    
+    if((value>113) && (value<=143)) 
+    emoji = '😀'
+
+    if((value>143) && (value<=180)) 
+    emoji = '🤩'
+    const content = 
+    `<div class="note-item__header">
+    <p class="note-item__date">${today}</p>
+    <p class="note-item__emoji" style="font-size: 1.5rem;">${emoji}</p>
+    </div>
+    <p class="note-item__value" readonly>${noteText}</p>`
+    noteLi.innerHTML = content
+    return noteLi
+}
+
+const loadNotes = () => {
+    const list = document.querySelector('.notes-list')
+    const previousNotes = JSON.parse(localStorage.getItem('data')) || []
+    list.innerHTML=""
+    previousNotes.forEach((note)=>{
+        list.appendChild(Note(note))
+    })
+}
+
+
+
+
+
